612c2510fb9a1bb884259041975f8fdcdb73d719
fab <fabrizio.salmi@gmail.com>
2017-11-04 12:04:42 +0100

    Create test.js

---
+++ examples/create_movie_from_website/test.js
@@ -0,0 +1,18 @@
+var page = require('webpage').create();
+page.viewportSize = { width: 640, height: 480 };
+
+page.open('https://website.domain/page.html', function () {
+  setTimeout(function() {
+    // Initial frame
+    var frame = 0;
+    // Add an interval every 25th second
+    setInterval(function() {
+      // Render an image with the frame name
+      page.render('frames/frame_'+(frame++)+'.png', { format: "png" });
+      // Exit after 50 images
+      if(frame > 50) {
+        phantom.exit();
+      }
+    }, 25);
+  }, 777);
+});
