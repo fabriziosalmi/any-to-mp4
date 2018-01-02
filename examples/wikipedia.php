281642a6022e10ae14f74958717dc476a4014e25
fab <fabrizio.salmi@gmail.com>
2018-01-02 23:12:11 +0100

    Update wikipedia.php
    Content filters updated.

--- examples/wikipedia.php
+++ examples/wikipedia.php
@@ -1,9 +1,7 @@
 <?php
-// get content and image from Wikipedia, TTS from Amazon and create a speech video
-// example use: subject = Wikipedia, paragraphs = 10, video mode = waves
-// available video modes: waves, image (additional modes are in progress..)
-
-// cli command: /usb/bin/php-cgi -f wikipedia.php query=Wikipedia pars=2 mode=waves
+// get content from Wikipedia title: Wikipedia
+// paragraphs to use: 10
+// command: /usb/bin/php-cgi -f wikipedia.php query=Wikipedia pars=2 mode=waves
 
 $vmode = $_GET["mode"];
 
@@ -14,8 +12,8 @@ if ($vmode != ("waves"||"image")) {
 
 $paragraphs = $_GET["pars"];
 $query = $_GET["query"];
-// more filters/checks needed here..
 $query = str_replace(" ", "_", $query);
+// more filters
 
 // wikipedia text
 $url = "https://it.wikipedia.org/w/api.php?format=json&action=query&redirect=1&prop=extracts&explaintext=1&titles=".$query;
@@ -31,10 +29,13 @@ $content_filter = array(" =="," ==="," ==="," ==="," =====");
 $content = str_replace($content_filter, ". ", $content);
 $content_filter = array("====","===","=="," =\n");
 $content = str_replace($content_filter, " ", $content);
-$content = preg_replace( '/\s+/', ' ', $content );
+$content = preg_replace( '/\s+/', ' ', $content);
+$content = preg_replace('/\([^\)]+\)/', '', $content);
+
 $a = explode(". ", $content);
 $a = array_slice($a, 0, $paragraphs);
 $content = implode('. ', $a);
+$content = str_replace("  ", " ", $content);
 
 // wikipedia image API
 $wiki_image_api = "https://it.wikipedia.org/w/api.php?action=query&redirect=1&titles=".$query."&prop=pageimages&format=json&pithumbsize=640";
@@ -53,11 +54,9 @@ $wiki_mp4 = "tmp/wiki_".$hash.".mp4";
 $wiki_json = "tmp/wiki_".$hash.".json";
 $wiki_mkv = "tmp/wiki_".$hash.".mkv";
 
-// write local storage
 file_put_contents($wiki_txt, $content);
 file_put_contents($wiki_img, $image_content_data);
 
-// JSON logging
 $media = array();
 $media["title"] = $query;
 $media["content"] = $content;
@@ -65,6 +64,7 @@ $media["image"] = $wiki_img;
 $media["mp3"] = $wiki_mp3;
 $media["mp4"] = $wiki_mp4;
 $media["json"] = $wiki_json;
+
 file_put_contents($wiki_json, $media);
 
 // text to mp3 (Amazon Polly)
@@ -73,38 +73,19 @@ exec("/usr/bin/perl txt2ssml.pl tmp/wiki2.txt > tmp/wiki.ssml");
 exec("/usr/bin/python ssml2mp3.py tmp/wiki.ssml -o ".$wiki_mp3);
 // alternatives: gtts-cli, txt2wav
 
-// video modes
 if ($vmode == "waves") {
   $ffmpeg = '/usr/bin/ffmpeg -hide_banner -loglevel panic -i '.$wiki_mp3.' -filter_complex "[0:a]showwaves=s=1280x720:mode=line:rate=25,format=yuv420p[v]" -map "[v]" -map 0:a '.$wiki_mp4;
   exec($ffmpeg);
 }
 
 if ($vmode == "image") {
-  $ffmpeg2 = '/usr/bin/ffmpeg -hide_banner -loglevel panic -loop 1 -i '.$wiki_img.' -i '.$wiki_mp3.' -shortest -tune stillimage -c:a aac -strict -2 -b:a 192k -pix_fmt yuv420p -shortest '.$wiki_mp4;
+  $ffmpeg2 = 'ffmpeg -loop 1 -i '.$wiki_img.' -i '.$wiki_mp3.' -shortest -tune stillimage -c:a aac -strict -2 -b:a 192k -pix_fmt yuv420p -shortest '.$wiki_mp4;
   exec($ffmpeg2);
-  $ffmpeg3 = '/usr/bin/ffmpeg -hide_banner -loglevel panic -i '.$wiki_mp4.' -vf scale=1280:720 tmp/resized_video.mp4';
+  $ffmpeg3 = 'ffmpeg -i '.$wiki_mp4.' -vf scale=1280:720 tmp/resized_video.mp4';
   exec($ffmpeg3);
   exec('/bin/mv tmp/resized_video.mp4 '.$wiki_mp4);
 }
 
-// logging to screen
 var_dump($media);
-
-// cli output example (same as json file content)
-// 
-// array(6) {
-//   ["title"]=>
-//   string(9) "Wikipedia"
-//   ["content"]=>
-//   string(406) "Wikipedia (pronuncia: vedi sotto) è un'enciclopedia online a contenuto libero, collaborativa, multilingue e gratuita, nata nel 2001, sostenuta e ospitata dalla Wikimedia Foundation, un'organizzazione non a scopo di lucro statunitense. Lanciata da Jimmy Wales e Larry Sanger il 15 gennaio 2001, inizialmente nell'edizione in lingua inglese, nei mesi successivi ha aggiunto edizioni in numerose altre lingue"
-//   ["image"]=>
-//   string(45) "tmp/wiki_6b71c8bcbf8b15e1c65196aeec16d776.png"
-//   ["mp3"]=>
-//   string(45) "tmp/wiki_6b71c8bcbf8b15e1c65196aeec16d776.mp3"
-//   ["mp4"]=>
-//   string(45) "tmp/wiki_6b71c8bcbf8b15e1c65196aeec16d776.mp4"
-//   ["json"]=>
-//   string(46) "tmp/wiki_6b71c8bcbf8b15e1c65196aeec16d776.json"
-// }
 
 ?>
